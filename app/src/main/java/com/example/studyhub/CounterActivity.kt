ef6f5896e83d1c7dbb88786def96bb3cb5eda85e
joaoluchetta <joaoluchetta.dev@gmail.com>
2025-09-16 22:11:37 -0300

    ADD meu projeto Counter ao Projeto-X

---
+++ app/src/main/java/com/example/studyhub/CounterActivity.kt
@@ -0,0 +1,81 @@
+package com.example.studyhub
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.studyhub.databinding.ActivityCounterBinding
+import com.example.studyhub.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+private var contador = 0
+
+class CounterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.rootCounter)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootCounter)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+//        val textContador: TextView = findViewById(R.id.textContador)
+//        val btnPlus: Button = findViewById(R.id.btnPlus)
+//        val btnMinus: Button = findViewById(R.id.btnMinus)
+
+        binding.btnPlus.setOnClickListener{ rootCounter ->
+            contador++
+            binding.textContador.text = contador.toString()
+        }
+
+        binding.btnMinus.setOnClickListener{ rootCounter ->
+            contador--
+            if(contador >= 0)
+                binding.textContador.text = contador.toString()
+            else {
+                contador = 0
+                //Toast.makeText(this, "Números negativos não são permitidos", Toast.LENGTH_SHORT).show()
+                Snackbar.make(rootCounter, "Numeros negativos não são permitidos", Snackbar.LENGTH_SHORT).show()
+            }
+            binding.textContador.text = contador.toString()
+        }
+
+        binding.btnVoltar.setOnClickListener { rootCounter ->
+            this.finish()
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d("LIFECICYLE", "ON_START" )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECICYLE", "ON_RESUME" )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECICYLE", "ON_PAUSE" )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECICYLE", "ON_STOP" )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECICYLE", "ON_DESTROY" )
+    }
+
+}
--- app/src/main/java/com/example/studyhub/MainActivity.kt
+++ app/src/main/java/com/example/studyhub/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.studyhub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.studyhub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnCounter.setOnClickListener {
+            val intentCounter = Intent(this, CounterActivity::class.java)
+            startActivity(intentCounter)
         }
     }
 }
