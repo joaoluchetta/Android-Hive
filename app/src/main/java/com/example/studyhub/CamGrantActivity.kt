bc87d814d2988cfd963ac497c78fbfd55af7670a
joaoluchetta <joaoluchetta.dev@gmail.com>
2025-09-18 16:02:00 -0300

    Removendo linhas do codigo que nao estavam sendo usadas

--- app/src/main/java/com/example/studyhub/CamGrantActivity.kt
+++ app/src/main/java/com/example/studyhub/CamGrantActivity.kt
@@ -4,17 +4,12 @@ package com.example.studyhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
